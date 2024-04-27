72932da24576e64d1158221a904573ec9ffc0160
Ritesh <ritzn95@gmail.com>
2024-04-27 15:10:30 +0530

    Working with NPM

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,6 @@
+// var generateName = require('sillyname');
+// import generateName from 'sillyname'
+import superheroes from 'superheroes'
+var superheroesName = superheroes.random();;
+
+console.log('I am ' + superheroesName + '!');
